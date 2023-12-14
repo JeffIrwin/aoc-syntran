a9f3eb2acbb886617fa73a99666ad1eda436a1b6
Jeff Irwin <jirwin505@gmail.com>
2023-12-13 20:34:51 -0500

    add automatic input downloader to newday.sh script

--- newday.sh
+++ newday.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# Provide year and date as single arg, e.g. "2023/13"
+#
+# TODO: arg count checking
+
 mkdir -p $1
 cp template.syntran $1/main.syntran
 
-## Doesn't work?
-#pushd $1
-##cd $1
+pushd $1
+
+year=${1%%/*}
+day=${1#*/}
+echo "year = $year"
+echo "day = $day"
+
+# The cookies text file can be downloaded via firefox addin "cookies.txt"
+curl --cookie ~/cookies.txt https://adventofcode.com/${year}/day/${day}/input -o input.txt
+
+popd
 
