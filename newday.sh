819583cec90465fbd3f08eb3fa02e3b32df49bb2
Jeff Irwin <jirwin505@gmail.com>
2024-12-01 01:06:59 -0500

    add comment on how to get cookies for input auto download

--- newday.sh
+++ newday.sh
@@ -2,7 +2,10 @@
 
 set -xe
 
-# Provide year and date as single arg, e.g. "2023/13"
+# Provide year and date as single arg, e.g. "2023/1" or "2023/13"
+#
+# For days 1 through 9, make sure to use a single digit.  If you try "01" you
+# will get a 404 error
 #
 # TODO: arg count checking
 
@@ -17,6 +20,13 @@ echo "year = $year"
 echo "day = $day"
 
 # The cookies text file can be downloaded via firefox addin "cookies.txt"
+#
+# To save cookies, click the puzzle-shaped icon in the top-right of Firefox to
+# access your extensions.  Then click on cookies.txt and "open extension" and
+# finally "current site" (or "ALL").  Take whichever file gets exported for AOC
+# and copy it to "~/cookies.txt", which is where the curl command below expects
+# to find your cookies
+#
 curl --cookie ~/cookies.txt https://adventofcode.com/${year}/day/${day}/input -o input.txt
 
 popd
