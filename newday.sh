fa35b29e9ebc89954801ef7a61686a4bac3a2b2b
Jeff Irwin <jirwin505@gmail.com>
2024-12-01 11:23:01 -0500

    improve newday.sh script

--- newday.sh
+++ newday.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-set -xe
+#set -xe
+set -e
+
+green="\e[92;1m"
+magenta="\e[95m"
+yellow="\e[93m"
+red="\e[91;1m"
+bold="\e[;1m"
+reset="\e[0m"
 
 # Provide year and date as single arg, e.g. "2023/1" or "2023/13"
 #
@@ -9,15 +17,28 @@ set -xe
 #
 # TODO: arg count checking
 
-mkdir -p $1
-cp template.syntran $1/main.syntran
-
-pushd $1
+if [[ "$#" -ne "1" ]]; then
+	echo -e "${red}Error${reset}: missing year/day cmd arg"
+	echo "Usage:"
+	echo "    newday.sh 2024/1"
+	exit -1
+fi
 
 year=${1%%/*}
 day=${1#*/}
 echo "year = $year"
 echo "day = $day"
+
+# Ban leading 0's because it will 404.  Could automatically remove
+if [[ "${day:0:1}" == "0" ]]; then
+	echo -e "${red}Error${reset}: cannot use leading 0's in day"
+	exit -1
+fi
+
+mkdir -p $1
+cp template.syntran $1/main.syntran
+
+pushd $1
 
 # The cookies text file can be downloaded via firefox addin "cookies.txt"
 #
@@ -27,7 +48,11 @@ echo "day = $day"
 # and copy it to "~/cookies.txt", which is where the curl command below expects
 # to find your cookies
 #
+# There are built-in browser features that can probably do this more easily
+#
 curl --cookie ~/cookies.txt https://adventofcode.com/${year}/day/${day}/input -o input.txt
 
 popd
+
+echo -e "${green}Done!${reset}"
 
