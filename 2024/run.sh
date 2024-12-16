e7475426a796d0fc67592a79fbb8fe018121741c
Jeff Irwin <jirwin505@gmail.com>
2024-12-15 19:50:44 -0500

    remove obsolete dict versions. syntran 0.0.58 is released

---
+++ 2024/run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#set -exu
+set -eu
+
+echo "starting run.sh"
+
+dirs=$( \
+	find . -type d -printf '%P\n' \
+	| sort -g\
+)
+
+for dir in ${dirs[@]} ; do
+	pushd "$dir" >/dev/null
+
+	main=main.syntran
+	grep 'Expect' "$main" | tr -d '\t'
+
+	#time syntran "$main"
+	syntran "$main"
+
+	echo
+	popd >/dev/null
+done
+
+echo "ending run.sh"
+
