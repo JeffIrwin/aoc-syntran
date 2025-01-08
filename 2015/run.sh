41d0e57f4928ea3898a5eb2d47c20b048d9f0899
Jeff Irwin <jirwin505@gmail.com>
2025-01-07 20:37:12 -0500

    add aoc 2015 day 16 and 17

---
+++ 2015/run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Go to every sub dir and run the aoc syntran script
+
+#set -exu
+set -eu
+
+echo "starting run.sh"
+
+dirs=$( \
+	find . -type d -printf '%P\n' \
+	| sort -g\
+)
+
+for dir in ${dirs[@]} ; do
+
+	# Part 1 is manageable but part 2 took like 20 minutes to run
+	[[ "$dir" == "4" ]] && continue
+
+	pushd "$dir" >/dev/null
+
+	main=main.syntran
+	grep 'Expect' "$main" | tr -d '\t'
+
+	time syntran "$main"
+	#syntran "$main"
+
+	echo
+	popd >/dev/null
+done
+
+echo "ending run.sh"
+
