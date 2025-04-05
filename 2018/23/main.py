57554169d6030c67ce3e169064a8d4dd43b204eb
Jeff Irwin <jirwin505@gmail.com>
2025-04-05 16:54:18 -0400

    add aoc 2018 day 23

---
+++ 2018/23/main.py
@@ -0,0 +1,24 @@
+import sys,re
+#from Queue import PriorityQueue
+from queue import PriorityQueue
+
+
+#bots = [map(int, re.findall("-?\d+", line)) for line in sys.stdin]
+bots = [map(int, re.findall("-?[0-9]+", line)) for line in sys.stdin]
+
+q = PriorityQueue()
+for x,y,z,r in bots:
+  d = abs(x) + abs(y) + abs(z)
+  q.put((max(0, d - r),1))
+  q.put((d + r + 1,-1))
+count = 0
+maxCount = 0
+result = 0
+while not q.empty():
+  dist,e = q.get()
+  count += e
+  if count > maxCount:
+    result = dist
+    maxCount = count
+print(result)
+
